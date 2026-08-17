6a4ce38e0bb884dbcf39086362288212e3e41c86
czhs <83367088+czhs@users.noreply.github.com>
2026-08-17 08:43:44 +0000

    Deploying to gh-pages from @ czhs/czhs.github.io@134af263a81fc18951a3f667e039c3789df7d199 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -253,9 +253,14 @@ ninja.data = [{
               window.location.href = "/projects/9_project/";
             },},{id: "research-ih-engineering-notebook",
           title: 'IH Engineering Notebook',
-          description: "raw notes from a day of VPD experiments — five decompositions of one model",
+          description: "exploring new techniques",
           section: "Research",handler: () => {
               window.location.href = "/research/ih-engineering-notebook-2026-08-15/";
+            },},{id: "research-ih-engineering-notebook",
+          title: 'IH Engineering Notebook',
+          description: "working with a toy model",
+          section: "Research",handler: () => {
+              window.location.href = "/research/ih-engineering-notebook-2026-08-16/";
             },},{
         id: 'social-github',
         title: 'GitHub',
